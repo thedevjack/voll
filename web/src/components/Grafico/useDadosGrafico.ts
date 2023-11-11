c354d1a852cb9ea83798e68ba4aa0c0fcd21d255
thedevjack <jackson1santos@hotmail.com>
2023-11-10 23:46:43 -0300

    inserindo server e web

---
+++ web/src/components/Grafico/useDadosGrafico.ts
@@ -0,0 +1,30 @@
+import IConsulta from "../../types/IConsulta";
+import IProfissional from "../../types/IProfissional";
+
+interface IDados {
+  nome: string;
+  consultas: number;
+}
+
+interface Props {
+  profissionais: IProfissional[] | undefined;
+  consultas: IConsulta[] | undefined;
+}
+
+const useDadosGrafico = ({ profissionais, consultas }: Props) => {
+  let dados: Array<IDados> = [];
+
+  if (profissionais && consultas) {
+    dados = profissionais.map((profissional) => ({
+      nome: profissional.nome,
+      consultas: consultas.filter((consulta) =>
+        consulta.profissional.some((prof) => prof.nome === profissional.nome)
+      ).length
+    }));
+  }
+
+
+  return dados;
+};
+
+export default useDadosGrafico;
